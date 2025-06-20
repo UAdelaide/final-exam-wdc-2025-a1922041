4115446e3aa659ab87c4ce6ce899606c3bcf8586
vivian <vivianchan113@gmail.com>
2025-06-20 12:13:45 +0000

    [automatic commit] 2025-06-20 12:13:45 UTC 118.210.186.19

--- part1/app.js
+++ part1/app.js
@@ -39,16 +39,14 @@ let db;
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       )
     `);
-    
-    // Users table
+
+    // Dogs table
     await db.execute(`
-        CREATE TABLE IF NOT EXISTS books (
-      user_id INT AUTO_INCREMENT PRIMARY KEY,
-      username VARCHAR(50) UNIQUE NOT NULL,
-      email VARCHAR(100) UNIQUE NOT NULL,
-      password_hash VARCHAR(255) NOT NULL,
-      role ENUM('owner', 'walker') NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    dog_id INT AUTO_INCREMENT PRIMARY KEY,
+    owner_id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    size ENUM('small', 'medium', 'large') NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES Users(user_id)
         )
       `);
 
