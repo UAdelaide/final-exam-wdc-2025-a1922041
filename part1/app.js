3c4668e67b9a685b441609d9104dd59c01987b99
vivian <vivianchan113@gmail.com>
2025-06-20 13:48:21 +0000

    [automatic commit] 2025-06-20 13:48:21 UTC 49.178.97.119

--- part1/app.js
+++ part1/app.js
@@ -109,7 +109,9 @@ let db;
 // Route to get dogs
 app.get('/api/dogs', async (req, res) => {
     try {
-      const [dogs] = await db.execute('SELECT * FROM dogs');
+      const [dogs] = await db.execute(`
+
+        'SELECT * FROM dogs'`);
       res.json(dogs);
     } catch (err) {
       res.status(500).json({ error: 'Failed to fetch dogs' });
