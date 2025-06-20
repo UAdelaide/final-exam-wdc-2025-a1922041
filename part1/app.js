7ef9bbf3b0ea09fdfaf58520dbaaeb15fb110d03
vivian <vivianchan113@gmail.com>
2025-06-20 13:37:24 +0000

    [automatic commit] 2025-06-20 13:37:24 UTC 49.178.97.119

--- part1/app.js
+++ part1/app.js
@@ -128,9 +128,20 @@ app.get('/api/dogs', async (req, res) => {
   // Route to get walkrequests open
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
-      const [walks] = await db.execute(
-        'SELECT * FROM WalkRequests WHERE status = 'open'');
-      res.json(walks);
+        const [requests] = await db.execute(`
+            SELECT
+              wr.request_id,
+              d.name AS dog_name,
+              wr.requested_time,
+              wr.duration_minutes,
+              wr.location,
+              u.username AS owner_username
+            FROM WalkRequests wr
+            JOIN Dogs d ON wr.dog_id = d.dog_id
+            JOIN Users u ON d.owner_id = u.user_id
+            WHERE wr.status = 'open'
+          `);
+          res.json(requests);
     } catch (err) {
       res.status(500).json({ error: 'Failed to fetch walkrequests for open' });
     }
