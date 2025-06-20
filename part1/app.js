bbbd65812efa4a1515f41687bd231aec290a3e33
vivian <vivianchan113@gmail.com>
2025-06-20 12:15:30 +0000

    [automatic commit] 2025-06-20 12:15:30 UTC 118.210.186.19

--- part1/app.js
+++ part1/app.js
@@ -65,17 +65,37 @@ let db;
         )
       `);
 
+
     // Walk Ratings table
     await db.execute(`
-        CREATE TABLE IF NOT EXISTS books (
-    request_id INT AUTO_INCREMENT PRIMARY KEY,
-    dog_id INT NOT NULL,
-    requested_time DATETIME NOT NULL,
-    duration_minutes INT NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
+    rating_id INT AUTO_INCREMENT PRIMARY KEY,
+    request_id INT NOT NULL,
+    walker_id INT NOT NULL,
+    owner_id INT NOT NULL,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    comments TEXT,
+    rated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
+    FOREIGN KEY (walker_id) REFERENCES Users(user_id),
+    FOREIGN KEY (owner_id) REFERENCES Users(user_id),
+    CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
+        )
+      `);
+
+
+    // Walk Ratings table
+    await db.execute(`
+    rating_id INT AUTO_INCREMENT PRIMARY KEY,
+    request_id INT NOT NULL,
+    walker_id INT NOT NULL,
+    owner_id INT NOT NULL,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    comments TEXT,
+    rated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
+    FOREIGN KEY (walker_id) REFERENCES Users(user_id),
+    FOREIGN KEY (owner_id) REFERENCES Users(user_id),
+    CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
         )
       `);
 
