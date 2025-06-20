765032b7f0bd7c576e6e306802b43bbec3c24082
vivian <vivianchan113@gmail.com>
2025-06-20 12:27:54 +0000

    [automatic commit] 2025-06-20 12:27:54 UTC 118.210.186.19

--- part1/app.js
+++ part1/app.js
@@ -128,8 +128,8 @@ app.get('/api/dogs', async (req, res) => {
   // Route to get walkrequests open
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
-      const [dogs] = await db.execute('SELECT * FROM dogs');
-      res.json(dogs);
+      const [walks] = await db.execute('SELECT * FROM dogs');
+      res.json(walks);
     } catch (err) {
       res.status(500).json({ error: 'Failed to fetch dogs' });
     }
