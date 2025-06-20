2d21332c6f5c385439405633842ed1f4a26e7fc0
vivian <vivianchan113@gmail.com>
2025-06-20 12:09:38 +0000

    [automatic commit] 2025-06-20 12:09:38 UTC 118.210.186.19

--- part1/app.js
+++ part1/app.js
@@ -1,15 +1,9 @@
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mysql = require('mysql2/promise');
+var path = require('mysql2');
 
 var app = express();
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 
 let db;
 
